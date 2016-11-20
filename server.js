b1c5688ec3c31b69ac0a68b5757f435c4598c6ff
rodrigo <rodrigoreyes658@gmail.com>
2016-11-20 10:28:49 -0600

    carpetas public y views

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express=require("express");//importamos el servidor de express
 var app=express();//ejecutamos el servidor de express
 server=require("http").Server(app);//crear servidor con http y pasarle la aplicacion express
 
+app.set("view engine","ejs");
+app.use("/estatico",express.static('public'));
+
 server.listen(8080,function(){
-  console.log("Server running, on port 8080");
+  console.log("Server running on port 8080");
 })
