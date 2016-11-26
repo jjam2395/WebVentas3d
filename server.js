c1d8083a4230875f91b3a745add86986219a08c0
rodrigo <rodrigoreyes658@gmail.com>
2016-11-26 12:55:52 -0600

    herencia de plantillas usando include

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express=require("express");//importamos el servidor de express
 var app=express();//ejecutamos el servidor de express
+var cookieSession=require("cookie-session");
 server=require("http").Server(app);//crear servidor con http y pasarle la aplicacion express
 
 app.set("view engine","ejs");
 app.use("/estatico",express.static('public'));
+app.use(cookieSession(
+{
+       name: "session",
+       keys: ["llave1","llave2"]
+}));
 
 
 app.get("/",function(req,res)
